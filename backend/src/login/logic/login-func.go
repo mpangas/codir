3191b8efd0f68bc91acb94290c7d212261ba12bc
tylerberndt03 <101296374+tylerberndt03@users.noreply.github.com>
2023-01-27 15:46:53 -0500

    more login

--- backend/src/login/logic/login-func.go
+++ backend/src/login/logic/login-func.go
@@ -1,7 +1,7 @@
 package login
 
 import (
-	//"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm"
 	"golang.org/x/crypto/bcrypt"
 
 	"encoding/json"
@@ -11,6 +11,13 @@ import (
 type UserInfo struct {
 	uname    string `json:"uname"`
 	password string `json:"password"`
+}
+
+var loginDb *gorm.DB
+
+func init() {
+	loginDb, _ = gorm.Open("mysql", "") // fill this in later
+	db.AutoMigrate(&Book{})
 }
 
 func Signup(w http.ResponseWriter, r *http.Request) {
@@ -25,14 +32,23 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 	hashPwd, _ := bcrypt.GenerateFromPassword([]byte(newInfo.password), 10)
 	newInfo.password = string(hashPwd)
 
-	// TODO: database (u+1f480)
+	// todo: prevent duplicate unames
+
+	loginDb.Create(newInfo)
 
 }
 
 func Signin(w http.ResponseWriter, r *http.Request) {
-	var checkInfo UserInfo
-	if err := json.NewDecoder(r.Body).Decode(&checkInfo); err != nil {
+	var requestInfo UserInfo
+	if err := json.NewDecoder(r.Body).Decode(&requestInfo); err != nil {
 		http.Error(w, "Malformed request", 400)
+		return
+	}
+
+	var checkInfo UserInfo
+
+	if err := bcrypt.CompareHashAndPassword([]byte(checkInfo.password), []byte(requestInfo.password)); err != nil {
+		http.Error(w, "Incorrect password", 401)
 		return
 	}
 
