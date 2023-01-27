3960767c2be32cb8abc9a2ff1fc8307c344e8914
tylerberndt03 <101296374+tylerberndt03@users.noreply.github.com>
2023-01-27 11:28:48 -0500

    started login

--- backend/src/login/logic/login-func.go
+++ backend/src/login/logic/login-func.go
@@ -1,1 +1,31 @@
 package login
+
+import (
+	//"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+
+	"encoding/json"
+	"net/http"
+)
+
+type UserInfo struct {
+	uname    string `json:"uname"`
+	password string `json:"password"`
+}
+
+func Signup(w http.ResponseWriter, r *http.Request) {
+	// turn json into user info
+	var newInfo UserInfo
+	json.NewDecoder(r.Body).Decode(&newInfo)
+
+	// turn password into hash
+	hashPwd, _ := bcrypt.GenerateFromPassword([]byte(newInfo.password), 10)
+	newInfo.password = string(hashPwd)
+
+	// TODO: database (u+1f480)
+
+}
+
+func Signin(w http.ResponseWriter, r *http.Request) {
+
+}
--- backend/src/login/routes/login-routes.go
+++ backend/src/login/routes/login-routes.go
@@ -2,11 +2,10 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
-	//"login/logic"
+	//"login/logic" need to do import path
 )
 
 func LoginRoutes(router *mux.Router) {
 	router.HandleFunc("/login/signup", logic.Signup)
 	router.HandleFunc("/login/signin", logic.Signin)
-	//router.HandleFunc("/login/")
 }
