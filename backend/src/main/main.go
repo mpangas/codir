fb5752a6c3ce238c30a53a9315e92cc81435dab0
tylerberndt03 <101296374+tylerberndt03@users.noreply.github.com>
2023-01-28 10:32:54 -0500

    main function

---
+++ backend/src/main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/mpangas/codir/backend/src/login/routes"
+)
+
+func main() {
+	router := mux.NewRouter()
+	routes.LoginRoutes(router)
+	// as more functionality is added, more routes will be added.
+
+	fmt.Printf("Starting server")
+	if err := http.ListenAndServe(":8000", router); err != nil {
+		log.Fatal(err)
+	}
+}
