85d2f6595b9037a0638f82b71b34429b90a151ba
ansh-kalariya <akalariya@ufl.edu>
2023-01-31 16:44:12 -0500

    Sign Up button created and styled

--- frontend/my-app/src/components/Header.tsx
+++ frontend/my-app/src/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
          <div className="header">
           <h1 id="logo">CODIR</h1>
           <button className="cred" id="login">LOGIN</button>
-          <button className="cred" id="signup">Signup</button>
+          <button className="cred" id="signup">SIGN UP</button>
          </div>
       );
 }
