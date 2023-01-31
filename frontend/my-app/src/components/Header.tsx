d7fc9b323fc093c3e9dd024437450e249b04da9b
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-01-31 15:29:42 -0500

    created components for landing page

--- frontend/my-app/src/components/Header.tsx
+++ frontend/my-app/src/components/Header.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 function Header() {
     return (
-        <div className="App">
          <div className="header">
           <h1 id="logo">CODIR</h1>
          </div>
-        </div>
       );
 }
 
