728e6c641f3bdce7aebfe56897a18d878316a52a
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-01-31 16:21:43 -0500

    updated css

--- frontend/my-app/src/components/Header.tsx
+++ frontend/my-app/src/components/Header.tsx
@@ -4,6 +4,8 @@ function Header() {
     return (
          <div className="header">
           <h1 id="logo">CODIR</h1>
+          <button className="cred" id="login">LOGIN</button>
+          <button className="cred" id="signup">Signup</button>
          </div>
       );
 }
