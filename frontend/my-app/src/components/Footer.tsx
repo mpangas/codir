b76e7dc63aa31ba2af549cb86aefff955621b8ea
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-02-01 17:59:36 -0500

    landing page update

--- frontend/my-app/src/components/Footer.tsx
+++ frontend/my-app/src/components/Footer.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Footer() {
     return (
-        <div className="footer"></div>
+        <div className="footer">
+            <div className="horz"></div>
+            <div className="git"></div>
+        </div>
       );
 }
 
--- frontend/my-app/src/components/Main.tsx
+++ frontend/my-app/src/components/Main.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 function Main() {
     return (
-        <div className="main"></div>
+        <div className="main">
+          <div id="rectangle">
+            <p id="bigText">TAKE YOUR SKILLS IN THE RIGHT DIRECTION</p>
+            <p id="smallText">Explore and expand your technical skills browsing video tutorials created by professors and renowned industry members.</p>
+          </div>
+          <div id="studyImg"></div>
+        </div>
       );
 }
 
