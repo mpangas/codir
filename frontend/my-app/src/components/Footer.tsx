5088937e6f2165b8a0941c1448c8c9bf1e5fe909
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-02-01 18:30:50 -0500

    hyperlink updated

--- frontend/my-app/src/components/Footer.tsx
+++ frontend/my-app/src/components/Footer.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import github from './image/github.png';
 function Footer() {
     return (
         <div className="footer">
             <div className="horz"></div>
-            <div className="git"></div>
+            <a href="https://github.com/mpangas/codir" target="_blank">
+                <img src={github} id="git"></img>
+            </a>
+            {/*<div className="git"></div>*/}
         </div>
       );
 }
