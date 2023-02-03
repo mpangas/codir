6ee29414a846a3b5d325fa54aecb34edfb29a93d
ansh-kalariya <akalariya@ufl.edu>
2023-02-03 17:18:47 -0500

    Creation of sign up page
    minor positioning changes need to be made.

--- frontend/my-app/src/Pages/Signup.tsx
+++ frontend/my-app/src/Pages/Signup.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
+
 function Signup() {
     return (
         <div className="main">
-          <div id="rectangle">
-            <p id="bigText">TA</p>
-            <p id="smallText">Explore and expand your technical skills browsing video tutorials created by professors and renowned industry members.</p>
+          <div id="largebox">
+            <p id="promptedtext">Sign up for your Codir account</p>
+            <p id="email">Email</p>
+            <p id="username">Username</p>
+            <p id="password">Password</p>
+            <p id="emailbox"></p>
+            <p id="usernamebox"></p>
+            <p id="passwordbox"></p>
+            <p id="signupbox">SIGN UP</p>
+            <p id="alreadyhaveaccount">already have an account? click here to sign in</p>
+
+            
+
           </div>
-          <div id="studyImg"></div>
         </div>
       );
 }
