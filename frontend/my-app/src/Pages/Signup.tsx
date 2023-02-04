d185a2ce70b77bc891722a86fdf40a74fb286ec0
ansh-kalariya <akalariya@ufl.edu>
2023-02-03 19:39:22 -0500

    Continued changes of sign up page

--- frontend/my-app/src/Pages/Signup.tsx
+++ frontend/my-app/src/Pages/Signup.tsx
@@ -6,13 +6,15 @@ function Signup() {
         <div className="main">
           <div id="largebox">
             <p id="promptedtext">Sign up for your Codir account</p>
+            
             <p id="email">Email</p>
             <p id="username">Username</p>
             <p id="password">Password</p>
-            <p id="emailbox"></p>
-            <p id="usernamebox"></p>
-            <p id="passwordbox"></p>
-            <p id="signupbox">SIGN UP</p>
+            <input id="emailbox"></input>
+            <input id="usernamebox"></input>
+            <input id="passwordbox"></input>
+            
+            <button id="signupbox">SIGN UP</button>
             <p id="alreadyhaveaccount">already have an account? click here to sign in</p>
 
             
