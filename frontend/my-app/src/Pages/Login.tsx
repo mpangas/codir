78782a7e917c5acbae37bbf87e4b89c46e45d716
ansh-kalariya <akalariya@ufl.edu>
2023-02-04 15:39:27 -0500

    creation of login page

--- frontend/my-app/src/Pages/Login.tsx
+++ frontend/my-app/src/Pages/Login.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 function Login() {
     return (
-        <div className="main">
-          <div id="rectangle">
-            <p id="bigText">TAKE Y RIGHT DIRECTION</p>
-            <p id="smallText">Explore and expand your technical skills browsing video tutorials created by professors and renowned industry members.</p>
-          </div>
-          <div id="studyImg"></div>
-        </div>
+      <div className="main">
+      <div id="mainloginbox">
+        <p id="promptedlogintext">Login to your Codir account</p>
+        <input className="boxMargin" id="loginusernamebox" placeholder='Enter your Username'></input>
+        <input type={'password'} className="boxMargin" id="loginpasswordbox" placeholder='Enter your Password'></input>
+        <button className="boxMargin" id="loginbox">LOGIN</button>
+      </div>
+    </div>
       );
 }
 
--- frontend/my-app/src/Pages/Signup.tsx
+++ frontend/my-app/src/Pages/Signup.tsx
@@ -8,9 +8,9 @@ function Signup() {
             <p id="promptedtext">Sign up for your Codir account</p>
             <input className="boxMargin" id="emailbox" placeholder='Enter your Email'></input>
             <input className="boxMargin" id="usernamebox" placeholder='Enter your Username'></input>
-            <input className="boxMargin" id="passwordbox" placeholder='Enter your Password'></input>
+            <input type={'password'} className="boxMargin" id="passwordbox" placeholder='Enter your Password'></input>
             <button className="boxMargin" id="signupbox">SIGN UP</button>
-    <p id="alreadyhaveaccount">already have an account? click here to sign in</p>
+            <p id="alreadyhaveaccount">already have an account? click here to sign in</p>
 
             
 
