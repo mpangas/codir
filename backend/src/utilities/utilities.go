bb229ba1dc3f0a507b5da51d9d5670e8051b4d5e
tylerberndt03 <101296374+tylerberndt03@users.noreply.github.com>
2023-02-06 15:52:40 -0500

    Added function for reading the json requests
    From the tutorial

---
+++ backend/src/utilities/utilities.go
@@ -0,0 +1,16 @@
+package utilities
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func ReadJson(r *http.Request, x interface{}) error {
+	if body, err := io.ReadAll(r.Body); err == nil {
+		if err := json.Unmarshal([]byte(body), x); err != nil {
+			return err
+		}
+	}
+	return nil
+}
