ac56531f43137859b888488b0c8c1b5540ef0101
Mike Pangas <mpangas@ufl.edu>
2023-02-12 15:46:45 -0500

    Implementing Login

--- test-frontend/react-auth/src/pages/Login.tsx
+++ test-frontend/react-auth/src/pages/Login.tsx
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const submit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const response = await fetch('http://localhost:8000/api/signin', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include',
+            body: JSON.stringify({
+                username,
+                password
+            })
+        })
+
+        const data = await response.json();
+        console.log(data);
+    }
+    
     return (
         <main className="form-signin w-100 m-auto">
-            <form>
+            <form onSubmit={submit}>
                 <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
                 <div className="form-floating">
-                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-                    <label htmlFor="floatingInput">Email address</label>
+                    <input className="form-control" id="floatingInput" placeholder="name@example.com" onChange={e => setUsername(e.target.value)}/>
+                    <label htmlFor="floatingInput">Username</label>
                 </div>
                 <div className="form-floating">
-                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-                    <label htmlFor="floatingPassword">Password</label>
+                    <input className="form-control" id="floatingPassword" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+                    <label htmlFor="floatingPassword">Password</label>  
                 </div>
                 <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
             </form>
--- test-frontend/react-auth/src/pages/Register.tsx
+++ test-frontend/react-auth/src/pages/Register.tsx
@@ -1,9 +1,11 @@
-import React, { SyntheticEvent, useState } from 'react'
+import { SyntheticEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const submit = async (e: SyntheticEvent) => {
         e.preventDefault();
@@ -16,10 +18,14 @@ const Register = () => {
                 username,
                 password
             })
-        });
+        })
         
-        const content = await response.json();
-        console.log(content);
+        const data = await response.json();
+        if(response.status === 200) {
+            navigate("/login");
+        } else {
+            console.log(data.message);
+        }
     }
     
     return (
@@ -27,8 +33,8 @@ const Register = () => {
             <form onSubmit={submit}>
                 <h1 className="h3 mb-3 fw-normal">Create an Account</h1>
                 <div className="form-floating">
-                    <input className="form-control" id="floatingInput" placeholder="name@example.com" onChange={e => setUsername(e.target.value)}/>
-                    <label htmlFor="floatingInput">Name</label>
+                    <input className="form-control" id="floatingInput" placeholder="Username" onChange={e => setUsername(e.target.value)}/>
+                    <label htmlFor="floatingInput">Username</label>
                 </div>
                 <div className="form-floating">
                     <input className="form-control" id="floatingEmail" placeholder="Email Address" onChange={e => setEmail(e.target.value)}/>
