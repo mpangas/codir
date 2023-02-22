119c0262390aa0a4264bf7cb880b78228bb2d150
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-02-22 15:15:51 -0500

    revised changes

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { useEffect } from 'react';
 import './CSS/App.css';
 import './CSS/Header.css';
 import './CSS/Footer.css';
@@ -12,16 +13,32 @@ import Main from './components/Main'
 import Footer from './components/Footer'
 import Login from './Pages/Login'
 import Signup from './Pages/Signup'
+import HomePage from './Pages/HomePage'
 
 function App() {
-  
+  const [username, setUsername] = React.useState('');
+  useEffect(() => {
+    (
+      async () => {
+        const response = await fetch('http://localhost:8000/api/user', {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+          credentials: 'include',
+        })
+
+        const data = await response.json();
+        setUsername(data.username);
+      }
+    )();
+  });
   return (
     <BrowserRouter>
     <div className="App">
-      <Header />
+      <Header username={username} setUsername={setUsername}/>
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<HomePage username={username} />} />
+          <Route path="/login" element={<Login setUsername={setUsername}/>} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
       <Footer />
--- frontend/my-app/src/Pages/Login.tsx
+++ frontend/my-app/src/Pages/Login.tsx
@@ -1,37 +1,57 @@
 import React from 'react';
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { TextField } from '@mui/material';
 import { spacing } from '@mui/system';
 import Button from '@mui/material/Button'
 
-function Login() {
-    const [user, setUser] = React.useState("");
-    const [pass, setPass] = React.useState("");
-    function userChange(e: React.ChangeEvent<HTMLInputElement>) {
-      setUser(e.target.value); 
-    }
-    function passChange(e: React.ChangeEvent<HTMLInputElement>) {
-      setPass(e.target.value); 
-    }
-    function printInput() {
-      console.log("Username: " + user);
-      console.log("Password: " + pass);
-    }
+const Login = (props: {setUsername: (username: string) => void }) => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const submit = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      const response = await fetch('http://localhost:8000/api/signin', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          credentials: 'include',
+          body: JSON.stringify({
+              username,
+              password
+          })
+      })
+
+      const data = await response.json();
+      if(response.status === 200) {
+          navigate("/");
+          props.setUsername(username);
+      } else {
+          console.log(data.message);
+      }
+  }
     return (
       <div className="main">
       <div id="mainloginbox">
+      <form onSubmit={submit}>
         <p id="promptedlogintext">Login to your Codir account</p>
-        <TextField sx={{ml: 9 , mb: 2, mt: -1}} id="loginusernamebox" label="Username" type="Username" />
+        <TextField 
+          sx={{ml: 9 , mb: 2, mt: -1}} 
+          id="loginusernamebox" 
+          label="Username" 
+          type="Username" 
+          onChange={e => setUsername(e.target.value)}
+        />
         <TextField
           sx={{ ml: 9 , mb: 0.5}}
           id="loginpasswordbox"
           label="Password"
           type="password"
           autoComplete="current-password"
+          onChange={e => setPassword(e.target.value)}
         />
-        {/*<input value={user} className="boxMargin" id="loginusernamebox" placeholder='Enter your Username' onChange={userChange}></input>*/}
-        {/*<input value={pass} type={'password'} className="boxMargin" id="loginpasswordbox" placeholder='Enter your Password' onChange={passChange}></input>*/}
-        {/*<button className="boxMargin" id="loginbox" onClick={printInput}>LOGIN</button>*/}
-        <Button variant="contained" color="primary" className="boxMargin" id="loginbox" onClick={printInput}>LOGIN</Button>
+        <Button variant="contained" color="primary" className="boxMargin" id="loginbox" type="submit">LOGIN</Button>
+      </form>
       </div>
     </div>
       );
