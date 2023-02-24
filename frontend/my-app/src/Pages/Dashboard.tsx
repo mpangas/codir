dfdfa0ed8c6b113b072c1cff90712557ffea4356
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-02-24 16:16:22 -0500

    card component setup

--- frontend/my-app/src/Pages/Dashboard.tsx
+++ frontend/my-app/src/Pages/Dashboard.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 
 const Dashboard = (props: {username: string}) => {
     const navigate = useNavigate();
@@ -12,6 +19,22 @@ const Dashboard = (props: {username: string}) => {
             <h1 id="welcome">Welcome, {props.username} !</h1> 
             <h2 className="uniform">Favorites</h2>
             <div className="uniform" id="horizontal"></div>
+            <div className="Card">
+                <Card sx={{ maxWidth: 345, marginTop: 1, marginLeft: 9 }}>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div" sx={{ display: 'flex', justifyContent: "center" }}>
+                            TITLE
+                        </Typography>
+                         <Typography variant="body2" color="text.secondary" sx={{ display: 'flex', justifyContent: "center" }}>
+                            AUTHOR
+                        </Typography>
+                     </CardContent>
+                     <CardMedia sx={{display: 'flex', float: "right", marginRight: 1}}>
+                        <ThumbUpOffAltIcon sx={{marginRight: 0.5}}></ThumbUpOffAltIcon>
+                        <p>98</p>
+                     </CardMedia>
+                </Card>
+            </div>
         </div>
     )
 }
