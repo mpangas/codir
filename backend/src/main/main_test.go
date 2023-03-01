4df15b251d87e51654e5183acebb6f6ddab15b77
Mike Pangas <mpangas@ufl.edu>
2023-02-28 23:05:24 -0500

    Added Signup unit test

--- backend/src/main/main_test.go
+++ backend/src/main/main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 	"testing"
@@ -28,7 +30,43 @@ func TestGet(t *testing.T) {
 	req, _ := http.NewRequest(http.MethodGet, "/api/get", nil)
 	resp, _ := app.Test(req, -1)
 
-	body, _ := ioutil.ReadAll(resp.Body)
+	body, _ := io.ReadAll(resp.Body)
+	defer resp.Body.Close()
+
+	t.Log(resp.StatusCode)
+	t.Log(string(body))
+	assert.Equal(t, 200, resp.StatusCode)
+}
+
+func TestSignup(t *testing.T) {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	pass := os.Getenv("DB_PASS")
+	database.Connect(pass)
+	app := fiber.New()
+
+	// Make mock request body
+	testUser := struct {
+		Email    string `json:"email"`
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}{
+		Email:    "test@gmail.com",
+		Username: "test",
+		Password: "password",
+	}
+
+	bodyReq, _ := json.Marshal(testUser)
+
+	app.Post("/api/signup", logic.Signup)
+
+	req, _ := http.NewRequest(http.MethodPost, "/api/signup", bytes.NewBuffer(bodyReq))
+	req.Header.Set("Content-Type", "application/json")
+	resp, _ := app.Test(req, -1)
+
+	body, _ := io.ReadAll(resp.Body)
 	defer resp.Body.Close()
 
 	t.Log(resp.StatusCode)
