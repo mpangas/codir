94abb97e9ea8d96c44248b09b9b26cd79e1e651b
Mike Pangas <mpangas@ufl.edu>
2023-02-28 21:31:39 -0500

    Implemented api/get unit test

--- backend/src/main/main_test.go
+++ backend/src/main/main_test.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/mpangas/codir/backend/src/database"
 	"github.com/mpangas/codir/backend/src/login/logic"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGet(t *testing.T) {
-	server := fiber.New()
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	pass := os.Getenv("DB_PASS")
+	database.Connect(pass)
+	app := fiber.New()
 
-	server.Get("/api/get", logic.GetUsers)
+	app.Get("/api/get", logic.GetUsers)
 
 	req, _ := http.NewRequest(http.MethodGet, "/api/get", nil)
-	resp, _ := server.Test(req, -1)
+	resp, _ := app.Test(req, -1)
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
 
 	t.Log(resp.StatusCode)
-	t.Log(resp.Body)
+	t.Log(string(body))
 	assert.Equal(t, 200, resp.StatusCode)
 }
