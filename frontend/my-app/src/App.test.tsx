7ae1c14bc7f5859c82a76aca6ac7ef048c798d8b
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-03-16 16:27:54 -0400

    successful test case

---
+++ frontend/my-app/src/App.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import App from './App';
+import Login from './Pages/Login'
+import jest from 'jest'
+
+test('test', () => {
+    expect(true).toBe(true);
+})
+
+
+
