b6333376a173c92b6b34948463bd35cca9319998
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-03-16 21:30:54 -0400

    update package.json

--- frontend/my-app/src/App.test.tsx
+++ frontend/my-app/src/App.test.tsx
@@ -1,17 +1,25 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import App from './App';
-import Login from './Pages/Signup'
-import jest from 'jest'
+import Login from './Pages/Login'
 import Signup from './Pages/Signup';
+import jest from 'jest'
+
 
 test('test', () => {
     expect(true).toBe(true);
 })
 
-test('button rendering', () => {
-  const { getByTestId } = render(<App/>);
-  const buttonElement = getByTestId('my-button');
-  expect(buttonElement).toBeInTheDocument();
-})
+test('displays error message when username is not entered', async () => {
+  const setUsername = jest.fn();
+  const { getByLabelText, getByText } = render(<Login setUsername={(username: string) => console.log(username)} />);
+  const usernameInput = getByLabelText('Username');
+  const passwordInput = getByLabelText('Password');
+  const loginButton = getByText('LOGIN');
 
+  fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
+  fireEvent.click(loginButton);
+
+  const errorMsg = await getByText('You must enter a username.');
+  expect(errorMsg).toBeInTheDocument();
+});
