8cc243bcd0d199f9c234b79854dd90ba4f6322b9
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-03-23 21:01:32 -0400

    few margins and font changes

--- frontend/my-app/src/components/Card.tsx
+++ frontend/my-app/src/components/Card.tsx
@@ -5,22 +5,23 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import { margin } from '@mui/system';
 
 function Card(props: any) {
     return(
     <div className="Card">
                 <Cards sx={{ width: 315, height: 260 , marginTop: 3, marginLeft: 15 }}>
                     <CardContent>
-                        <Typography gutterBottom variant="h5" component="div" sx={{ display: 'flex', justifyContent: "center" }}>
+                        <Typography gutterBottom variant="h5" component="div" sx={{ display: 'flex', justifyContent: "center", fontSize: 30 }}>
                             {props.title}
                         </Typography>
-                         <Typography variant="body2" color="text.secondary" sx={{ display: 'flex', justifyContent: "center" }}>
+                         <Typography variant="body2" color="text.secondary" sx={{ display: 'flex', justifyContent: "center", fontSize: 15 }}>
                             {props.author}
                         </Typography>
                      </CardContent>
-                     <CardMedia sx={{display: 'flex', float: "right", marginRight: 1}}>
-                        <ThumbUpOffAltIcon sx={{marginRight: 0.5}}></ThumbUpOffAltIcon>
-                        <p>{props.likes}</p>
+                     <CardMedia sx={{display: 'flex', float: "right", marginTop: 15, marginRight: 1.5}}>
+                        <ThumbUpOffAltIcon sx={{}}></ThumbUpOffAltIcon>
+                        <p id="numLikes">{props.likes}</p>
                      </CardMedia>
                 </Cards>
         </div>
