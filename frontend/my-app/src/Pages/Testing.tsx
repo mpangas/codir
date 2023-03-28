77be1e39d2fff382c8064b25a68e9c58624ece07
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-03-27 23:10:16 -0400

    margins are set for the testing page

--- frontend/my-app/src/Pages/Testing.tsx
+++ frontend/my-app/src/Pages/Testing.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { FormControl, TextField, InputLabel, OutlinedInput, InputAdornment, IconButton, Button } from '@mui/material';
+import { spacing, Stack } from '@mui/system';
 
 const Testing = (props: { username: string }) => {
     const [Title, setTitle] = useState("");
     const [Location, setLocation] = useState("");
     const [User, setUser] = useState("");
-    
+
     const submit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const response = await fetch('http://localhost:8000/api/tutorials', {
@@ -24,27 +25,28 @@ const Testing = (props: { username: string }) => {
     }
     return (
         <div className="testing">
-            <br></br>
             <form onSubmit={submit}>
-                <TextField
-                    sx={{ ml: 50, mb: 2, mt: 2 }}
-                    className="input-box"
-                    label="Title"
-                    onChange={e => setTitle(e.target.value)}
-                />
-                <TextField
-                    sx={{ ml: 50, mb: 2, mt: -1 }}
-                    className="input-box"
-                    label="Location"
-                    onChange={e => setLocation(e.target.value)}
-                />
-                <TextField
-                    sx={{ ml: 50, mb: 2, mt: -1 }}
-                    className="input-box"
-                    label="User"
-                    onChange={e => setUser(e.target.value)}
-                />
-                <Button sx={{ ml: 50, mb: 2, mt: -1 }} variant="contained" color="primary" className="boxMargin" id="loginbox" type="submit">Submit</Button>
+                <FormControl sx={{ml: 70}} >
+                    <TextField
+                        sx={{mt: 4}}
+                        className="input-box"
+                        label="Title"
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <TextField
+                        sx={{mt: 4}}
+                        className="input-box"
+                        label="Location"
+                        onChange={e => setLocation(e.target.value)}
+                    />
+                    <TextField
+                        sx={{mt: 4}}
+                        className="input-box"
+                        label="User"
+                        onChange={e => setUser(e.target.value)}
+                    />
+                    <Button sx={{mt: 4}} variant="contained" color="primary" type="submit">Submit</Button>
+                </FormControl>
             </form>
         </div>
     );
