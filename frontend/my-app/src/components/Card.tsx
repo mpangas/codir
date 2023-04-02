8643eaadd8c967169b52297b203df88ed6878a3c
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-04-02 13:14:41 -0400

    score setup changed

--- frontend/my-app/src/components/Card.tsx
+++ frontend/my-app/src/components/Card.tsx
@@ -6,6 +6,8 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import StarIcon from '@mui/icons-material/Star';
+import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
+import ThumbDownAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
 import { margin } from '@mui/system';
 
 function Card(props: any) {
@@ -17,12 +19,13 @@ function Card(props: any) {
                             {props.title}
                         </Typography>
                          <Typography variant="body2" color="text.secondary" sx={{ display: 'flex', justifyContent: "center", fontSize: 15 }}>
-                            {props.author}
+                            {props.user}
                         </Typography>
                      </CardContent>
                      <CardMedia sx={{display: 'flex', float: "right", marginTop: 15, marginRight: 1.5}}>
-                        <StarIcon sx={{}}></StarIcon>
-                        <p id="numLikes">{props.likes}</p>
+                        <ThumbUpAltOutlinedIcon sx={{marginRight: 0.5}}></ThumbUpAltOutlinedIcon>
+                        <Typography gutterBottom variant="h5" component="div" sx={{marginRight: 1, marginTop: 0.2}}>{props.score}</Typography>
+                        <ThumbDownAltOutlinedIcon sx={{marginRight: 0.5, marginTop: 0.2}}></ThumbDownAltOutlinedIcon>
                      </CardMedia>
                 </Cards>
         </div>
