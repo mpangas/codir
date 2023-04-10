602956f9c61295ad1409f148361fa93a1a68a213
tylerberndt03 <101296374+tylerberndt03@users.noreply.github.com>
2023-04-10 12:25:33 -0400

    Change attributes to align to frontend

--- backend/src/models/attributes.go
+++ backend/src/models/attributes.go
@@ -2,9 +2,8 @@ package models
 
 type Attributes struct {
 	// yeah idk what attributes we want yet
-	SkillLevel  string `"json:skillLevel"`  // Beginner, Intermediate, Experienced
-	Language    string `"json:language"`    // Python, C++, Go, etc.
-	Application string `"json:application"` // frontend, backend, ai/ml, gamedev, etc.
-	Duration    string `"json:duration"`
-	// these aren't the only attributes that could be and are just to
+	SkillLevel string `"json:skillLevel"` // Beginner, Intermediate, Experienced
+	Language   string `"json:language"`   // Python, C++, Go, etc.
+	Technology string `"json:technology"` // .net, angular, etc.
+	Style      string `"json:style"`      // text, interactive, video
 }
