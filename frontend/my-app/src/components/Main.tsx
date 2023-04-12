a35f018d43046577761cfc9a9f7abb40e4533b95
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-04-12 18:17:28 -0400

    promotional card components added

--- frontend/my-app/src/components/Main.tsx
+++ frontend/my-app/src/components/Main.tsx
@@ -2,9 +2,17 @@ import { Box, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import StudyPic from '../image/study.png'
-import { textAlign } from '@mui/system';
+import { textAlign, width } from '@mui/system';
 import { green } from '@mui/material/colors';
-import Transparent from '../image/transparent.png'
+import Transparent from '../image/transparent.png';
+import Preferences from '../image/preferences.png';
+import Browsing from '../image/browsing.png';
+import Filter from '../image/filter.png';
+import Recommendation from '../image/recommendation.png';
+import Cards from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 const Main = (props: { username: string }) => {
   /*const navigate = useNavigate();
@@ -27,9 +35,51 @@ const Main = (props: { username: string }) => {
       </Stack>
       <Typography gutterBottom variant="h6" component="div" sx={{ textAlign: 'center', marginTop: 8, fontSize: 22 }}>Get introduced and learn modern developer tools, frameworks, programming languages
         <br /> through our top-rated tutorials</Typography>
-      <Box sx={{float: 'left', marginLeft: 60}}>
+      <Box sx={{ display: "flex", marginLeft: 2 }}>
         <img src={Transparent} id="transparentImg"></img>
       </Box>
+      <Typography gutterBottom variant="h6" component="div" sx={{ textAlign: 'center', marginTop: 6, fontSize: 22 }}>We guarantee the best user experience with effective learning techniques</Typography>
+      <Stack direction="row">
+        <Cards sx={{ width: 315, height: 260, marginTop: 5, color: 'black', mx: 5, borderRadius: 5, boxShadow: '0px 2.75px 2.75px rgba(0, 0, 0, .75)' }}>
+          <CardContent>
+          <Typography gutterBottom variant="body1" component="div" sx={{ textAlign: 'center', fontSize: 22 }}> 
+          Choosing your preferences </Typography>
+          </CardContent>
+          <CardMedia>
+            <img src={Preferences} id="preferences"></img>
+          </CardMedia>
+        </Cards>
+
+        <Cards sx={{ width: 315, height: 260, marginTop: 5, color: 'black', mx: 5, borderRadius: 5, boxShadow: '0px 2.75px 2.75px rgba(0, 0, 0, .75)' }}>
+          <CardContent sx={{textAlign: "center"}}>
+          <Typography gutterBottom variant="body1" component="div" sx={{ textAlign: 'center', fontSize: 22 }}> 
+          Browsing through Tutorials </Typography>
+          </CardContent>
+          <CardMedia>
+            <img src={Browsing} id="browsing"></img>
+          </CardMedia>
+        </Cards>
+        
+        <Cards sx={{ width: 315, height: 260, marginTop: 5, color: 'black', mx: 5, borderRadius: 5, boxShadow: '0px 2.75px 2.75px rgba(0, 0, 0, .75)' }}>
+          <CardContent sx={{textAlign: "center"}}>
+          <Typography gutterBottom variant="body1" component="div" sx={{ textAlign: 'center', fontSize: 22, marginBottom: -1 }}> 
+          Getting Tutorial Recommendations </Typography>
+          </CardContent>
+          <CardMedia>
+            <img src={Recommendation} id="recommendations"></img>
+          </CardMedia>
+        </Cards>
+
+        <Cards sx={{ width: 315, height: 260, marginTop: 5, color: 'black', mx: 5, borderRadius: 5, boxShadow: '0px 2.75px 2.75px rgba(0, 0, 0, .75)' }}>
+          <CardContent sx={{textAlign: "center"}}>
+          <Typography gutterBottom variant="body1" component="div" sx={{ textAlign: 'center', fontSize: 22 }}> 
+          Filtering tutorials </Typography>
+          </CardContent>
+          <CardMedia>
+            <img src={Filter} id="filtering"></img>
+          </CardMedia>
+        </Cards>
+      </Stack>
     </div>
   );
 }
