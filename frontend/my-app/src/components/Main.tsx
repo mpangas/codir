c16a074aa596df061645b9d3a773e9d493515c72
PRANETALLU <92874312+PRANETALLU@users.noreply.github.com>
2023-04-12 17:07:25 -0400

    pic setup

--- frontend/my-app/src/components/Main.tsx
+++ frontend/my-app/src/components/Main.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import StudyPic from '../image/study.png'
@@ -19,15 +19,17 @@ const Main = (props: { username: string }) => {
     </div>*/}
       <Stack direction="row" justifyContent="center">
         <Stack direction="column" marginRight={6} marginTop={7}>
-          <Typography gutterBottom variant="h5" component="div" sx={{ fontSize: 58, fontFamily: 'Open Sans', color: "#028299"}}>TAKE YOUR SKILLS IN <br /> THE RIGHT DIRECTION</Typography>
+          <Typography gutterBottom variant="h5" component="div" sx={{ fontSize: 58, fontFamily: 'Open Sans', color: "#028299" }}>TAKE YOUR SKILLS IN <br /> THE RIGHT DIRECTION</Typography>
           <Typography gutterBottom variant="h6" component="div" sx={{ fontFamily: 'Open Sans', marginLeft: 1, fontSize: 19, color: "black" }}>Explore and expand your technical skills browsing video
             tutorials <br /> created by professors and renowned industry members.</Typography>
         </Stack>
         <img src={StudyPic} id="studyImg"></img>
       </Stack>
-      <Typography gutterBottom variant="h6" component="div" sx={{textAlign: 'center', marginTop: 8, fontSize: 22}}>Get introduced and learn modern developer tools, frameworks, programming languages 
-      <br/> through our top-rated tutorials</Typography>
-      <img src={Transparent} ></img>
+      <Typography gutterBottom variant="h6" component="div" sx={{ textAlign: 'center', marginTop: 8, fontSize: 22 }}>Get introduced and learn modern developer tools, frameworks, programming languages
+        <br /> through our top-rated tutorials</Typography>
+      <Box sx={{float: 'left', marginLeft: 60}}>
+        <img src={Transparent} id="transparentImg"></img>
+      </Box>
     </div>
   );
 }
