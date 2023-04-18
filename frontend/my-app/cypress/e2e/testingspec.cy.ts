d205e0cec364255ca1ebbee60413444ab0a62697
ansh-kalariya <akalariya@ufl.edu>
2023-04-18 04:55:24 -0400

    added some cypress tests

--- frontend/my-app/cypress/e2e/testingspec.cy.ts
+++ frontend/my-app/cypress/e2e/testingspec.cy.ts
@@ -53,7 +53,7 @@ describe('Login and browse navigation', () => {
     cy.get('button[id="loginbox"]').click()
     cy.get('.MuiButton-root').contains("BROWSE").click() 
     cy.url().should('include', 'http://localhost:3000/browse')
-    cy.get('h1').should('contain', 'Browse Tutorials')
+    cy.contains('Browse Tutorials').should('exist')
   });
 });
 
@@ -69,17 +69,6 @@ describe('Login and about page navigation', () => {
   });
 });
 
-// describe('Login and test navigation', () => {
-//   it('should login to the account then navigate to test page', () => {
-//     cy.visit('http://localhost:3000/login');
-//     cy.get('input[type="username"]').type('sample');
-//     cy.get('input[type="password"]').type('sample');
-//     cy.get('button[id="loginbox"]').click()
-//     cy.get('.MuiButton-root').contains("TEST").click() 
-//     cy.url().should('include', 'http://localhost:3000/test')
-//     cy.get('h1').should('contain', 'TEST')
-//   });
-// });
 
 describe('Login and preferences navigation', () => {
   it('should login to the account then navigate to preferences page', () => {
@@ -89,6 +78,62 @@ describe('Login and preferences navigation', () => {
     cy.get('button[id="loginbox"]').click()
     cy.get('.MuiButton-root').contains("PREFERENCES").click() 
     cy.url().should('include', 'http://localhost:3000/preferences')
-    cy.get('h1').should('contain', 'Search Preferences')
+    cy.contains('Search Preferences').should('exist')
+  });
+});
+
+describe('Submit tutorial button functionality', () => {
+  it('The submit tutorial button should be able to be clicked on and show the necessary text and functionality', () => {
+    cy.visit('http://localhost:3000/login');
+    cy.get('input[type="username"]').type('sample');
+    cy.get('input[type="password"]').type('sample');
+    cy.get('button[id="loginbox"]').click()
+    cy.get('.MuiButton-root').contains("BROWSE").click() 
+    cy.get('.MuiButton-root').contains("SUBMIT TUTORIAL").click() 
+    cy.contains('Find a great tutorial? Enter the details below!').should('exist')
+  });
+});
+
+describe('first drop down menu functionality', () => {
+  it('ensure all the necessary options are visible on the first drop down menu on the browse page', () => {
+    cy.visit('http://localhost:3000/login');
+    cy.get('input[type="username"]').type('sample');
+    cy.get('input[type="password"]').type('sample');
+    cy.get('button[id="loginbox"]').click()
+    cy.get('.MuiButton-root').contains("BROWSE").click() 
+    cy.get('.MuiSelect-select.MuiSelect-outlined.MuiInputBase-input.MuiOutlinedInput-input.MuiInputBase-inputSizeSmall.css-jedpe8-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input').contains("All Languages").click()
+    cy.contains('Assembly').should('exist')
+    cy.contains('Bash/Shell').should('exist')
+    cy.contains('C').should('exist')
+    cy.contains('C#').should('exist')
+    cy.contains('C++').should('exist')
+    cy.contains('COBOL').should('exist')
+    cy.contains('Dart').should('exist')
+    cy.contains('Elixir').should('exist')
+    cy.contains('F#').should('exist')
+    cy.contains('Fortran').should('exist')
+    cy.contains('Go').should('exist')
+    cy.contains('Groovy').should('exist')
+    cy.contains('Haskell').should('exist')
+    cy.contains('HTML/CSS').should('exist')
+    cy.contains('Java').should('exist')
+    cy.contains('JavaScript').should('exist')
+    cy.contains('Julia').should('exist')
+    cy.contains('Kotlin').should('exist')
+    cy.contains('Lua').should('exist')
+    cy.contains('MATLAB').should('exist')
+    cy.contains('OCaml').should('exist')
+    cy.contains('Perl').should('exist')
+    cy.contains('PHP').should('exist')
+    cy.contains('PowerShell').should('exist')
+    cy.contains('Python').should('exist')
+    cy.contains('R').should('exist')
+    cy.contains('Ruby').should('exist')
+    cy.contains('Rust').should('exist')
+    cy.contains('Scala').should('exist')
+    cy.contains('SQL').should('exist')
+    cy.contains('Swift').should('exist')
+    cy.contains('TypeScript').should('exist')
+    cy.contains('VBA').should('exist')
   });
 });
