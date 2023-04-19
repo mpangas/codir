7f350b1b13b4a14950dfcdf2aaa0d07e4441ac3e
Mike Pangas <mpangas@ufl.edu>
2023-04-18 21:51:20 -0400

    Fixed Preference Page bugs

--- frontend/my-app/src/Pages/Preferences.tsx
+++ frontend/my-app/src/Pages/Preferences.tsx
@@ -1,62 +1,24 @@
 import {
     Button,
     Typography,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    DialogTitle,
     FormControl,
-    TextField,
     Box,
     Container,
-    DialogContentText,
-    Grid,
-    Checkbox,
-    FormControlLabel,
-    FormGroup,
     InputLabel,
     MenuItem,
     Select,
     SelectChangeEvent,
-    ListItemText,
 } from '@mui/material';
-import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import FilterButton from '../components/FilterButton';
-import Card from '../components/Card';
-
-const FormFields = ({
-    Title,
-    setTitle,
-    Location,
-    setLocation,
-    User,
-    setUser,
-}: {
-    Title: string,
-    setTitle: (title: string) => void,
-    Location: string,
-    setLocation: (location: string) => void,
-    User: string,
-    setUser: (user: string) => void,
-}) => {
-
-};
 
 const Preferences = (props: { username: string }) => {
-    // Submit Tutorial
     const navigate = useNavigate();
 
-   // const [languages, setLanguage] = useState("All Languages");
-   // const [technologies, setTechnology] = useState("All Technologies");
-    // const [skillLevel, setDifficulty] = useState("All Skill Levels");
-    // const [styles, setLearningStyle] = useState("All Learning Styles");
-
     const [language, setoption] = useState<string[]>([]);
-    const [technologie, setoption2] = useState<string[]>([]);
+    const [technologies, setoption2] = useState<string[]>([]);
     const [style, setoption3] = useState<string[]>([]);
-    const [skillLeve, setoption4] = useState<string[]>([]);
+    const [skillLevel, setoption4] = useState('');
 
     useEffect(() => {
         if (props.username === "" || props.username === undefined) {
@@ -67,9 +29,9 @@ const Preferences = (props: { username: string }) => {
     const handleSubmit = async () => {
         const username = props.username;
         console.log(username);
-        var skillLevel = skillLeve.join(",");
+        var skill = skillLevel;
         var languages = language.join(",");
-        var technologies = technologie.join(",");
+        var tech = technologies.join(",");
         var styles = style.join(",");
         const response = await fetch('http://localhost:8000/api/preferences', {
             method: 'PUT',
@@ -77,9 +39,9 @@ const Preferences = (props: { username: string }) => {
             credentials: 'include',
             body: JSON.stringify({
                 username,
-                skillLevel,
+                skill,
                 languages,
-                technologies,
+                tech,
                 styles,
             }),
         });
@@ -87,19 +49,11 @@ const Preferences = (props: { username: string }) => {
         setoption([]);
         setoption2([]);
         setoption3([]);
-        setoption4([])
+        setoption4('');
     };
 
-    const handleDifficultyChange = (event: SelectChangeEvent<string[]>) => {
-        const newstuff4 = event.target.value as string[];
-
-        if (newstuff4.length < skillLeve.length) {
-            const remove4 = skillLeve.find((value) => !newstuff4.includes(value))!;
-            setoption4(skillLeve.filter((value) => value !== remove4));
-        } else {
-            const newthing4 = newstuff4.find((value) => !skillLeve.includes(value))!;
-            setoption4([...skillLeve, newthing4]);
-        }
+    const handleDifficultyChange = (event: SelectChangeEvent) => {
+        setoption4(event.target.value as string);
     };
 
     const handleLanguageChange = (event: SelectChangeEvent<string[]>) => {
@@ -116,12 +70,12 @@ const Preferences = (props: { username: string }) => {
     const handleTechnologyChange = (event: SelectChangeEvent<string[]>) => {
         const newstuff2 = event.target.value as string[];
 
-        if (newstuff2.length < technologie.length) {
-            const remove2 = technologie.find((value) => !newstuff2.includes(value))!;
-            setoption2(technologie.filter((value) => value !== remove2));
+        if (newstuff2.length < technologies.length) {
+            const remove2 = technologies.find((value) => !newstuff2.includes(value))!;
+            setoption2(technologies.filter((value) => value !== remove2));
         } else {
-            const newthing2 = newstuff2.find((value) => !technologie.includes(value))!;
-            setoption2([...technologie, newthing2]);
+            const newthing2 = newstuff2.find((value) => !technologies.includes(value))!;
+            setoption2([...technologies, newthing2]);
         }
     };
     const handleLearningStyleChange = (event: SelectChangeEvent<string[]>) => {
@@ -155,141 +109,90 @@ const Preferences = (props: { username: string }) => {
                 <Typography variant="h5" component="div" sx={{ display: 'flex', justifyContent: "center", fontSize: 16, marginTop: 5 }}>
                     Welcome to the preferences section! Please select your following preferences/options to best match your learning interests! Click submit to proceed to the Dashboard.
                 </Typography>
-            </Box>
 
-            <Box sx={{ display: 'flex', ml: '5%', my: 2, width: 200, justifyContent: 'center' }}>
-                <Box sx={{
-                    flex: 5,
-                    transform: 'translateX(-50%)',
-                    display: 'flex',
-                    position: 'absolute',
-                    top: '32%',
-                    left: '50%',
-                    justifyContent: 'center', zIndex: 1
-                }}>
-                    <FormControl sx={{ minHeight: 110, minWidth: 180, }}>
-                        <InputLabel id="langs">Programming Languages</InputLabel>
-                        <Select
-                            labelId="langs"
-                            multiple
-                            value={language}
-                            onChange={handleLanguageChange}
-                            renderValue={(selected) => selected.join(', ')}
-                        >
+                <FormControl sx={{ minWidth: 250, mt: 5 }}>
+                    <InputLabel id="langs-label">Programming Languages</InputLabel>
+                    <Select
+                        labelId="langs-label"
+                        id="langs"
+                        multiple
+                        value={language}
+                        label="Programming Languages"
+                        onChange={handleLanguageChange}
+                        renderValue={(selected) => selected.join(', ')}
+                    >
+                        {['Assembly', 'Bash/Shell', 'C', 'C#', 'C++', 'COBOL', 'Dart', 'Elixir', 'F#', 'Fortran', 'Go', 'Groovy', 'Haskell', 'HTML/CSS', 'Java', 'JavaScript', 'Julia', 'Kotlin', 'Lua', 'MATLAB', 'OCaml', 'Perl', 'PHP', 'PowerShell', 'Python', 'R', 'Ruby', 'Rust', 'Scala', 'SQL', 'Swift', 'TypeScript', 'VBA',].map((option) => (
+                            <MenuItem key={option} value={option}>
+                                {option}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
 
-                            {['Assembly', 'Bash/Shell', 'C', 'C#', 'C++', 'COBOL', 'Dart', 'Elixir', 'F#', 'Fortran', 'Go', 'Groovy', 'Haskell', 'HTML/CSS', 'Java', 'JavaScript', 'Julia', 'Kotlin', 'Lua', 'MATLAB', 'OCaml', 'Perl', 'PHP', 'PowerShell', 'Python', 'R', 'Ruby', 'Rust', 'Scala', 'SQL', 'Swift', 'TypeScript', 'VBA',].map((option) => (
-                                <MenuItem key={option} value={option}>
-                                    {option}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </Box>
-                <Box sx={{
-                    flex: 5,
-                    position: 'absolute',
-                    transform: 'translateX(-50%)',
-                    display: 'flex',
-                    top: '42%',
-                    left: '50%',
-                    justifyContent: 'center', zIndex: 1
-                }}>
-                    <FormControl sx={{ minHeight: 110, minWidth: 180, }}>
-                        <InputLabel id="prog-tech">Technologies</InputLabel>
-                        <Select
-                            labelId="prog-tech"
-                            multiple
-                            value={technologie}
-                            onChange={handleTechnologyChange}
-                            renderValue={(selected) => selected.join(', ')}
-                        >
+                <FormControl sx={{ minWidth: 250, mt: 3 }}>
+                    <InputLabel id="prog-tech">Technologies</InputLabel>
+                    <Select
+                        labelId="prog-tech"
+                        multiple
+                        value={technologies}
+                        label="Technologies"
+                        onChange={handleTechnologyChange}
+                        renderValue={(selected) => selected.join(', ')}
+                    >
+                        {['.NET', 'Angular', 'Angular.js', 'Ansible', 'ASP.NET', 'Blazor', 'Cloud Computing', 'CouchDB', 'Django', 'Docker', 'DynamoDB', 'Express', 'FastAPI', 'Flask', 'Flutter', 'Git', 'GitHub', 'GitLab', 'Homebrew', 'jQuery', 'Kubernetes', 'Laravel', 'MariaDB', 'Microsoft SQL Server', 'MongoDB', 'MySQL', 'Next.js', 'Node.js', 'npm', 'NumPy', 'Nuxt.js', 'Oracle', 'Pandas', 'PostgreSQL', 'PyTorch', 'Qt', 'React Native', 'React.js', 'Redis', 'Ruby on Rails', 'SQLite', 'Spring', 'Svelte', 'Terraform', 'TensorFlow', 'Unity 3D', 'Unreal Engine', 'Vue.js', 'Yarn'].map((options2) => (
+                            <MenuItem key={options2} value={options2}>
+                                {options2}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
 
-                            {['.NET', 'Angular', 'Angular.js', 'Ansible', 'ASP.NET', 'Blazor', 'Cloud Computing', 'CouchDB', 'Django', 'Docker', 'DynamoDB', 'Express', 'FastAPI', 'Flask', 'Flutter', 'Git', 'GitHub', 'GitLab', 'Homebrew', 'jQuery', 'Kubernetes', 'Laravel', 'MariaDB', 'Microsoft SQL Server', 'MongoDB', 'MySQL', 'Next.js', 'Node.js', 'npm', 'NumPy', 'Nuxt.js', 'Oracle', 'Pandas', 'PostgreSQL', 'PyTorch', 'Qt', 'React Native', 'React.js', 'Redis', 'Ruby on Rails', 'SQLite', 'Spring', 'Svelte', 'Terraform', 'TensorFlow', 'Unity 3D', 'Unreal Engine', 'Vue.js', 'Yarn'].map((options2) => (
-                                <MenuItem key={options2} value={options2}>
-                                    {options2}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </Box>
-                <Box sx={{
-                    flex: 5,
-                    transform: 'translateX(-50%)',
-                    display: 'flex',
-                    position: 'absolute',
-                    top: '52%',
-                    left: '50%',
-                    justifyContent: 'center', zIndex: 1
-                }}>
-                    <FormControl sx={{ minHeight: 110, minWidth: 180, }}>
-                        <InputLabel id="prog-style">Learning Styles</InputLabel>
-                        <Select
-                            labelId="prog-style"
-                            multiple
-                            value={style}
-                            onChange={handleLearningStyleChange}
-                            renderValue={(selected) => selected.join(', ')}
-                        >
+                <FormControl sx={{ minWidth: 250, mt: 3 }}>
+                    <InputLabel id="prog-style">Learning Styles</InputLabel>
+                    <Select
+                        labelId="prog-style"
+                        multiple
+                        label="Learning Styles"
+                        value={style}
+                        onChange={handleLearningStyleChange}
+                        renderValue={(selected) => selected.join(', ')}
+                    >
+                        {['Text Tutorials', 'Video Tutorials', 'Interactive Tutorials'].map((options3) => (
+                            <MenuItem key={options3} value={options3}>
+                                {options3}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
 
-                            {['Text Tutorials', 'Video Tutorials', 'Interactive Tutorials'].map((options3) => (
-                                <MenuItem key={options3} value={options3}>
-                                    {options3}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </Box>
-            </Box>
+                <FormControl sx={{ minWidth: 250, mt: 3 }}>
+                    <InputLabel id="prog-diff">Skill Levels</InputLabel>
+                    <Select
+                        labelId="prog-diff"
+                        label="Skill Levels"
+                        value={skillLevel}
+                        onChange={handleDifficultyChange}
+                    >
+                        {['Beginner', 'Intermediate', 'Advanced'].map((options4) => (
+                            <MenuItem key={options4} value={options4}>
+                                {options4}
+                            </MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
 
-            <Box sx={{
-                flex: 5,
-                transform: 'translateX(-50%)',
-                display: 'flex',
-                position: 'absolute',
-                top: '62%',
-                left: '50%',
-                justifyContent: 'center', zIndex: 1
-            }}>
-                <FormControl sx={{ minHeight: 110, minWidth: 180, }}>
-                        <InputLabel id="prog-diff">Skill Levels</InputLabel>
-                        <Select
-                            labelId="prog-diff"
-                            multiple
-                            value={skillLeve}
-                            onChange={handleDifficultyChange}
-                            renderValue={(selected) => selected.join(', ')}
-                        >
-                            {['Beginner', 'Intermediate', 'Advanced'].map((options4) => (
-                                <MenuItem key={options4} value={options4}>
-                                    {options4}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-            </Box>
-
-
-
-            <Box
-                height="105vh"
-                position="relative"
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-            >
                 {/*<Link to="/dashboard" style={{ textDecoration: "none" }}>*/}
                 <Button variant="contained" sx={{
                     backgroundColor: "#0097b2",
                     '&:hover': {
                         backgroundColor: "#028299",
                     },
-                    width: 150
+                    width: 150,
+                    mt: 5
                 }} onClick={handleSubmit}>
                     SUBMIT
                 </Button>{/*</Link>*/}
-
             </Box>
-
         </Container>
 
 
