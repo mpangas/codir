68eb09084a737695c9c3f3775d5993b0585ac8e6
Mike Pangas <mpangas@ufl.edu>
2023-04-19 16:01:19 -0400

    Cleaned up comments

--- backend/src/posts/routes/tutorials-routes.go
+++ backend/src/posts/routes/tutorials-routes.go
@@ -23,21 +23,11 @@ import (
 )
 
 func TutorialsRoutes(app *fiber.App) {
-	// CRUD
-	/*router.HandleFunc("/tutorials", logic.GetAllTutorials).Methods("GET")
-	router.HandleFunc("/tutorials/{id}", logic.GetTutorial).Methods("GET")
-	router.HandleFunc("/tutorials", logic.PostTutorial).Methods("POST")
-	router.HandleFunc("/tutorials/{id}", logic.EditTutorial).Methods("PUT") // what should be editable?
-	router.HandleFunc("/tutorials/{id}", logic.DeleteTutorial).Methods("DELETE")*/
-
 	app.Get("/api/tutorials", logic.GetAllTutorials)
 	app.Get("/api/tutorials/id::id", logic.GetTutorial)
 	app.Post("/api/tutorials", logic.PostTutorial)
 	app.Put("/api/tutorials/id::id", logic.EditTutorial)
 	app.Delete("/api/tutorials/id::id", logic.DeleteTutorial)
-
-	// I'm not yet 100% sure whether score increments should be their own routes, but this function would be common enough
-	// and simple enough that we shouldn't need to call EditTutorial every time.
 
 	app.Put("/api/tutorials/id::id/up", logic.VoteUp)
 	app.Put("/api/tutorials/id::id/down", logic.VoteDown)
